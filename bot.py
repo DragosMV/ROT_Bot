138202bc50d5a2d6c3eded3751c679f267082d9a
DragosMV <dragosmosteanu12@gmail.com>
2022-09-01 14:05:05 +0300

    Multiple changes Modified rbm command Modified issues command Modified welcome message Added exception command Added command about giant characters Added logging for deleted and edited messages

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import random
 from discord.ext.commands.cooldowns import BucketType
+import datetime
 
 
 description = '''A discord bot used in the Realm of Thrones Bannerlord mod community.
@@ -42,8 +43,8 @@ https://github.com/bennyz/Bannerlord.Diplomacy/releases/tag/v1.1.11.4""")
 # RBM info text command
 @commands.cooldown(1,6,BucketType.user)
 @bot.command()
-async def RBM(ctx):
-    await ctx.send("""RBM AI module is compatible with ROT. Do NOT use the combat module with ROT
+async def rbm(ctx):
+    await ctx.send("""RBM AI module is compatible with ROT. Do NOT use the combat module with ROT (disable it in main menu after loading up the game)
 https://www.nexusmods.com/mountandblade2bannerlord/mods/791""") 
 
 # ServeAsSoldier info text command
@@ -75,9 +76,8 @@ async def culture(ctx, *choices: str):
 async def issues(ctx):
     await ctx.send("""If you are having issues with running the mod please make sure you have followed these steps:
 1. Installed the mod as presented in this video with the correct load order https://youtu.be/3EceOVpsg84
-2. Disabled ANY other mods apart from the ones related to ROT as seen in the video above
-3. Make sure you are using the correct version of the game and the mod
-If you are still having problems, feel free to let us know in one of the help channels under the support category""") 
+2. Make sure you are using the correct version of the game and the mod
+If you are still having problems, you can perform a clean reinstall of everything and follow the video provided""") 
 
 # Gives a welcome message when a new user joins
 
@@ -89,15 +89,54 @@ async def launcher(ctx):
     await ctx.send("""If you are using more mods try using the BUTRLoader Launcher. Details in link below:
 https://www.nexusmods.com/mountandblade2bannerlord/mods/2513""") 
 
+
+# Command about launcher
+@commands.cooldown(1,6,BucketType.user)
+@bot.command()
+async def exception(ctx):
+    await ctx.send("""Better exception window provides more detailed error reports:
+https://www.nexusmods.com/mountandblade2bannerlord/mods/404""") 
+
+# Command about launcher
+@commands.cooldown(1,6,BucketType.user)
+@bot.command()
+async def giantbug(ctx):
+    await ctx.send("""Unusual, giant characters are usually caused by the hot butter mod""") 
+
+
 @bot.event
 async def on_member_join(member):
         guild = member.guild
         if guild.system_channel is not None:
             to_send = f'''Welcome {member.mention} to the {guild.name} discord server!
 We're glad to have you here, if you want to get started with the mod check out this video from Jackie on how to install it together with other mods:
-https://youtu.be/3EceOVpsg84. To see what commands the bot has available you can use ?help.
-The prefix for commands is a "?". ex: ?diplomacy to find out more about the diplomacy mod with ROT'''
+***To see what commands the bot has available you can use ?help***
+The prefix for commands is a "?". ex: ?diplomacy to find out more about the diplomacy mod with ROT
+https://youtu.be/3EceOVpsg84'''
             await guild.system_channel.send(to_send)
+
+# logs edited messages
+@bot.event
+async def on_message_edit(message_before,message_after):
+    if message_before.author.bot:
+        return
+    embed = discord.Embed(colour=0xe74c3c, timestamp=datetime.datetime.now())
+    embed.set_author(name=f"{message_before.author} edited a message",icon_url=message_before.author.display_avatar)
+    embed.add_field(name="Before:",value=f"{message_before.content}",inline=False)
+    embed.add_field(name="After:",value=f"{message_after.content}")
+    channel = bot.get_channel(1014587687287656468)
+    await channel.send(embed=embed)
+
+# logs deleted messages
+@bot.event
+async def on_message_delete(message):
+    if message.author.bot:
+        return
+    embed = discord.Embed(colour=0xe74c3c, timestamp=datetime.datetime.now())
+    embed.set_author(name=f"{message.author} deleted a message",icon_url=message.author.display_avatar)
+    embed.add_field(name="Deleted message",value=f"{message.content}",inline=False)
+    channel = bot.get_channel(1014587687287656468)
+    await channel.send(embed=embed)
 
 
 bot.run('TOKEN')
